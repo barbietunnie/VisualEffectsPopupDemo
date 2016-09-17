fba16d2e04985288d29115fb55a59d26f2bcd67f
Babatunde Adeyemi <barbietunnie@gmail.com>
2016-09-17 08:06:03 +0100

    Initial commit

---
+++ VisualEffectsPopupDemo/ViewController.swift
@@ -0,0 +1,66 @@
+//
+//  ViewController.swift
+//  VisualEffectsPopupDemo
+//
+//  Created by Babatunde Adeyemi on 9/17/16.
+//  Copyright © 2016 Babatunde Adeyemi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var addItemView: UIView!
+    @IBOutlet weak var visualEffectView: UIVisualEffectView!
+    var effect: UIVisualEffect!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        effect = visualEffectView.effect
+        visualEffectView.effect = nil
+        
+        addItemView.layer.cornerRadius = 5
+    }
+    
+    func animateIn() {
+        self.view.addSubview(addItemView)
+        addItemView.center = self.view.center
+        
+        addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+        addItemView.alpha = 0
+        
+        UIView.animate(withDuration: 0.4) { 
+            self.visualEffectView.effect = self.effect
+            self.addItemView.alpha = 1
+            self.addItemView.transform = CGAffineTransform.identity
+        }
+    }
+    
+    func animateOut() {
+        UIView.animate(withDuration: 0.3, animations: { 
+            self.addItemView.transform = CGAffineTransform.init(scaleX: 1.3, y: 1.3)
+            self.addItemView.alpha = 0
+            
+            self.visualEffectView.effect = nil
+        }) { (success: Bool) in
+                self.addItemView.removeFromSuperview()
+        }
+    }
+
+    @IBAction func addItem(_ sender: AnyObject) {
+        animateIn()
+    }
+    
+    @IBAction func dismissPopup(_ sender: AnyObject) {
+        animateOut()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
